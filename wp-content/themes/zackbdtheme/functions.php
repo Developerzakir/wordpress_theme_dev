f6a558ce37e593303e4e8893f02a127ba684aa30
zakir Hossen <zakirhossen082@gmail.com>
2025-04-29 16:57:38 +0600

    enqueue scripts & custimizer register

---
+++ wp-content/themes/zackbdtheme/functions.php
@@ -0,0 +1,49 @@
+<?php
+/*
+* My Theme Function
+*/
+
+
+// Theme Title
+add_theme_support('title-tag');
+
+
+// Theme CSS and jQuery File calling
+function zackbth_css_js_file_calling(){
+  wp_enqueue_style('zackbth-style', get_stylesheet_uri());
+  wp_register_style('bootstrap', get_template_directory_uri().'/css/bootstrap.css', array(), '5.0.2', 'all');
+  wp_register_style('custom', get_template_directory_uri().'/css/custom.css', array(), '1.0.0', 'all');
+  wp_enqueue_style('bootstrap');
+  wp_enqueue_style('custom');
+
+
+  //jQuery
+  wp_enqueue_script('jquery');
+  wp_enqueue_script('bootstrap', get_template_directory_uri().'/js/bootstrap.js', array(), '5.0.2', 'true' );
+  wp_enqueue_script('main', get_template_directory_uri().'/js/main.js', array(), '1.0.0', 'true' );
+
+}
+add_action('wp_enqueue_scripts', 'zackbth_css_js_file_calling');
+
+
+//Theme Function
+function zackbth_customizar_register($wp_customize){
+  $wp_customize->add_section('zackbth_header_area', array(
+    'title' =>__('Header Area', 'zackbth'),
+    'description' => 'If you interested to update your header area, you can do it here.'
+  ));
+
+  $wp_customize->add_setting('zackbth_logo', array(
+    'default' => get_bloginfo('template_directory') . '/img/logo.png',
+  ));
+
+  $wp_customize-> add_control(new WP_Customize_Image_Control($wp_customize, 'zackbth_logo', array(
+    'label' => 'Logo Upload',
+    'description' => 'If you interested to change or update your logo you can do it.',
+    'setting' => 'zackbth_logo',
+    'section' => 'zackbth_header_area',
+  ) ));
+
+}
+
+add_action('customize_register', 'zackbth_customizar_register');
