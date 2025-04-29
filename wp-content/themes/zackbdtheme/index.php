0c77cf583dde9d8f4bc46e8cedd8b951848020d8
zakir Hossen <zakirhossen082@gmail.com>
2025-04-29 18:33:50 +0600

    Create a walker menu

--- wp-content/themes/zackbdtheme/index.php
+++ wp-content/themes/zackbdtheme/index.php
@@ -19,7 +19,11 @@
           <a href=""><img src="<?php echo get_theme_mod('zackbth_logo'); ?>" alt=""></a>
         </div>
         <div class="col-md-9">
-        <?php wp_nav_menu( array('theme_location' => 'main_menu', 'menu_id' => 'nav') ); ?>
+        <?php wp_nav_menu( array(
+            'theme_location'  => 'main_menu',
+             'menu_id'        => 'nav',
+             'walker'         => new Description_Walker(),
+             ) ); ?>
         </div>
       </div>
     </div>
