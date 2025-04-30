6bb5c33e84ab7c2eb6f397105a7bcdbcaccd7b4b
zakir Hossen <zakirhossen082@gmail.com>
2025-04-30 12:07:08 +0600

    Working with WP 404 Pages

--- wp-content/themes/zackbdtheme/404.php
+++ wp-content/themes/zackbdtheme/404.php
@@ -1,10 +1,22 @@
 <?php
 /*
-* The main template file
+* The template for displaying 404 page (not found)
 */ 
 get_header(); ?>
 
-<h1>this is 404 page</h1>
+  <section id="body_area">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12 error_page">
+            <p>404 Error - Page not Found</p>
+            <h1>Oops! Looks Like something was wrong</h1>
+            <div class="error_search">
+                <?php get_search_form(); ?>
+            </div>
+            <a href="<?php echo home_url(); ?>" class="homepage">Homepage</a>
+        </div>
+      </div>
+    </div>
+  </section>
 
-
-<?php get_footer(); ?>
+  <?php get_footer(); ?>
