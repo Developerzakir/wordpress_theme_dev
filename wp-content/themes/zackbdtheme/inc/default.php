ddbda50db2bd11632a7a15e5054dc4ef3869a3c8
zakir Hossen <zakirhossen082@gmail.com>
2025-04-30 21:06:23 +0600

    add jQuery slider Plugin in theme

--- wp-content/themes/zackbdtheme/inc/default.php
+++ wp-content/themes/zackbdtheme/inc/default.php
@@ -8,6 +8,7 @@ add_theme_support('title-tag');
 add_theme_support( 'post-thumbnails', array('page', 'post','service') );
 add_image_size('post-thumbnails', 970, 350, true);
 add_image_size('service', 390, 250, true);
+add_image_size('slider', 1920, 600, true);
 add_theme_support( 'threaded-comments' );
 
 
